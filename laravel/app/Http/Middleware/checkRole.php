1464fab0602fe52ea19a604552afa73691a7bc41
laoderidho <2081007@unai.edu>
2023-10-08 15:51:06 +0700

    add authorization role

---
+++ laravel/app/Http/Middleware/checkRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class checkRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role)
+    {
+        $user = Auth::user();
+
+        if ($user->role === $role) {
+            return $next($request);
+        }
+        return response()->json(['message' => 'You are not authorized to access this route'], Response::HTTP_UNAUTHORIZED);
+    }
+}
