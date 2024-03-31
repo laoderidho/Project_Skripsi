e77612ce3e134088ef3b8b7c8ee9b11560ef5aa9
Bennedi <siregarhabel@gmail.com>
2024-03-31 15:54:20 +0700

    Update Implementasi

--- laravel/app/Http/Controllers/Perawat/StandarForm/InputImplementasiController.php
+++ laravel/app/Http/Controllers/Perawat/StandarForm/InputImplementasiController.php
@@ -11,12 +11,14 @@ use App\Models\Perawat\StandarForm\Form_Implementasi;
 use App\Models\Admin\Declension;
 use App\Models\Admin\Perawat;
 use App\Models\Perawat\StandarForm\Form_Evaluasi;
+use Carbon\Carbon;
 // auth suport
 use Illuminate\Support\Facades\Auth;
 // validator suport
 use Illuminate\Support\Facades\Validator;
 // db suport
 use Illuminate\Support\Facades\DB;
+
 class InputImplementasiController extends Controller
 {
     public function getIndex($id){
@@ -27,62 +29,74 @@ class InputImplementasiController extends Controller
         ]);
     }
 
-    public function checkList(Request $request){
-        dd($request->all());
-        // $users = Auth::user()->id;
-        // $perawat = Perawat::where('id_user',$users)->first();
-        // $perawat = $perawat->id;
-
-        // $validator = Validator::make($request->all(),[
-        //     'id'=>'required',
-        // ]);
-        // if ($validator->fails()) {
-        //     return response()->json($validator->errors()->toJson(), 400);
-        // }
-        // $id_implementasi = $request->id;
-        // $id_implementasi = explode(',', $id_implementasi);
-
-        // dd($id_implementasi);
-        // transaction db
-        // DB::beginTransaction();
-        // try{
-
-        // }
-        // catch (\Exception $e)
-        // {
-        //     DB::rollback();
-        //     return response()->json([
-        //     'message' => 'Failed',
-        //     'error' => $e->getMessage(), ],
-        //     500); }
-    }
-
-        public function getImplementasiPasien($id_pemeriksaan){
-
-            $pemeriksaan = Pemeriksaan::find($id_pemeriksaan);
-
-            if ($pemeriksaan == null) {
-                return response()->json([
-                    'message' => 'Pemeriksaan tidak ditemukan',
-                ], 404);
-            }
-
-            // select
-
-            $form_evaluasi = Form_Implementasi::where('id_pemeriksaan', $id_pemeriksaan)->whereNull('jam_ceklis')->get();
-
-            dd($form_evaluasi);
-
-            if ($form_evaluasi->isEmpty()) {
-                return response()->json([
-                    'message' => 'Data tidak ditemukan',
-                ], 404);
-            }
-
+    public function isDone($id){
+        $implementasi = Form_Implementasi::where('id_pemeriksaan', $id)->where('tindakan_implementasi','!=',1)->get();
+        if($implementasi->isEmpty()){
             return response()->json([
-                'message' => 'Success',
-                'data' => $form_evaluasi,
+                'message' => 'Semua implementasi telah dilakukan'
             ]);
         }
-}
 
+        return response()->json([
+            'message' => 'Success',
+            'data' => $implementasi
+        ]);
+    }
+
+
+
+    public function checkList(Request $request,$id){
+        $implementasi = Form_Implementasi::where('id',$id)->first();
+
+        $validator = Validator::make($request->all(), [
+            'tindakan_implementasi' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'data' => $validator->errors(),
+            ], 400);
+        }
+
+        if($request->tindakan_implementasi == 1){
+            $implementasi->jam_ceklis = Carbon::now()->format('H:i:s');
+        }else{
+            $implementasi->jam_ceklis=null;
+        }
+        $implementasi->tindakan_implementasi = $request->tindakan_implementasi;
+        $implementasi->update();
+
+        return response()->json([
+            'message' => 'Success',
+            'data' =>$implementasi
+        ]);
+    }
+
+
+    public function getImplementasiPasien($id_pemeriksaan)
+    {
+        $pemeriksaan = Pemeriksaan::find($id_pemeriksaan);
+
+        if ($pemeriksaan == null) {
+            return response()->json([
+                'message' => 'Pemeriksaan tidak ditemukan',
+            ], 404);
+        }
+
+        $form_implementasi = Form_Implementasi::where('id_pemeriksaan', $id_pemeriksaan)
+            ->whereNull('jam_ceklis')
+            ->get();
+
+        if ($form_implementasi->isEmpty()) {
+            return response()->json([
+                'message' => 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => $form_implementasi,
+        ]);
+    }
+}
